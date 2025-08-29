6b3dd029243b996860cad92ebb817ac15168be94
York Yu <n11453559@gmail.com>
2025-08-29 21:06:15 +1000

    Fix author display in Book component by removing unnecessary uppercase transformation

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const Book = () => {
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <h4>{author.toUpperCase}</h4>
+      <h4>{author}</h4>
     </article>
   )
 }
