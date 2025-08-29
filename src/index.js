6ffb9e8ed06d03dd49d01248e84a3cd7906bc87d
York Yu <n11453559@gmail.com>
2025-08-29 20:25:49 +1000

    Replace Greeting component with Booklist and implement Book, Image, Title, and Author components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function Greeting() {
+function Booklist() {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   )
 }
 
-const Person = () => <h1>John Doe</h1>
-const Message = () => {
-  return <p>This is my first React component!</p>
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/81JJPDNlxSL._AC_UL480_FMwebp_QL65_.jpg"
+    alt="The Silent Patient: The Recordr"
+  />
+)
+const Title = () => <h2>The Silent Patient: The Recordr</h2>
+const Author = () => {
+  return <h2>Alex Michaelides</h2>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Greeting />)
+root.render(<Booklist />)
