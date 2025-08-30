c525372e98cb12be5a732e43863da2987305363f
York Yu <n11453559@gmail.com>
2025-08-30 21:54:14 +1000

    Refactor Booklist component to use an array of book objects and streamline rendering

--- src/index.js
+++ src/index.js
@@ -2,38 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-const firstBook = {
-  author: 'Jordan Moore',
-  title: 'Interesting Facts For Curious Minds',
-  img: './images/book-1.jpg',
-}
-const secondBook = {
-  author: 'James Clear',
-  title: 'Atomic Habits',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
-}
+const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: './images/book-1.jpg',
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+    id: 2,
+  },
+]
 
 function Booklist() {
   return (
     <section className="booklist">
-      <Book book={firstBook} id={1}>
-        <p>Best Book Ever</p>
-      </Book>
-      <Book book={secondBook} id={2}>
-        <p>Another Great Book</p>
-      </Book>
+      {books.map((book) => (
+        <Book key={book.id} book={book}>
+          <p>{book.title}</p>
+        </Book>
+      ))}
     </section>
   )
 }
 
-const Book = ({ book, children }) => {
-  const { author, title, img } = book
+const Book = ({ book: { author, title, img } }) => {
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      <h2>{children}</h2>
     </article>
   )
 }
